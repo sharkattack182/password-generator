bf5e7dd6df0ac7be67bc3ae23890b113ed413544
Matthew Courtney <matthewdc182@gmail.com>
2020-08-01 13:26:40 -0400

    setting up the function starting with length prompt if prompt is not selected user will be reprompted

--- script.js
+++ script.js
@@ -22,7 +22,22 @@ var numbersYN;
 
 //Arrays
 
-var lowercaseOpts = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var uppercaseOpts = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var specialOpts = ["+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^", "~", "*", "?", ":"]
-var numberOpts = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var lowercaseOpts = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseOpts = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialOpts = ["+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^", "~", "*", "?", ":"];
+var numberOpts = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+//Functions
+
+function generatePassword() {
+    lengthValue = prompt("Please select a password length between 8 and 128 characters.");
+    
+    if (lengthValue == null) {
+        alert("Tis field is required");
+    } else if (lengthValue < 8 || lengthValue > 128) {
+            alert("Password must be between 8 and 128 characters")
+            lengthValue = prompt("Please select a password length between 8 and 128 characters.");
+        }
+        
+}
+    
